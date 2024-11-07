bb362b3e9e1f08d223759c58c6e76f1fa61b2b25
emanameen9982773 <emanothard15@gmail.com>
2024-11-08 02:58:18 +0300

    hii

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, girls!,eman,almaha,ghaida ");
+        System.out.println("Hello, girls!,eman,almaha,ghaida finally ");
     }
 }
